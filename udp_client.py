f187eabbd2f3fbbb9ff594353e9e46e0fd619cb3
Alex Michalak <atmichalak@gmail.com>
2023-04-01 00:48:32 -0400

    modified udp_client.py and udp_server.py

--- udp_client.py
+++ udp_client.py
@@ -1,28 +1,42 @@
-import socket
 import os
+import socket
 
-# Prompt the user to enter the server's IP address
-UDP_IP = input("Enter the server's IP address: ")
+def main():
+    # Set up the server's IP address and port number
+    UDP_IP = "192.168.0.210"
+    UDP_PORT = 8080
 
-#Set up the server's port number
-UDP_PORT = 8080
+    # Create a socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Create a socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Open the image file and get its size
+    filename = "test.jpg"
+    file_size = os.path.getsize(filename)
 
-# Open the image file and get its size
-filename = "test.jpg"
-file_size = os.path.getsize(filename)
+    # Send the image file size to the server
+    sock.sendto(str(file_size).encode(), (UDP_IP, UDP_PORT))
 
-# Send the image file size to the server
-sock.sendto(str(file_size).encode(), (UDP_IP, UDP_PORT))
-
-# Send the image file data to the server
-with open(filename, "rb") as f:
-    data = f.read(1024)
-    while data:
-        sock.sendto(data, (UDP_IP, UDP_PORT))
+    # Send the image file data to the server
+    with open(filename, "rb") as f:
+        sequence_number = 0
         data = f.read(1024)
+        while data:
+            packet = str(sequence_number).zfill(4).encode() + data
+            sock.sendto(packet, (UDP_IP, UDP_PORT))
+            print(f"Sent packet {sequence_number}")
+            sequence_number += 1
 
-sock.close()
-print("Image sent successfully")
+            # Wait for an acknowledgement from the server
+            ack_received = False
+            while not ack_received:
+                data, addr = sock.recvfrom(1024)
+                if data.decode() == "ACK":
+                    ack_received = True
+
+            data = f.read(1024)
+
+    sock.close()
+    print("Image sent successfully")
+
+if __name__ == "__main__":
+    main()
--- udp_server.py
+++ udp_server.py
@@ -1,44 +1,55 @@
+import os
 import socket
 import sys
 
-# Get the host machine's IP address
-hostname = socket.gethostname()
-UDP_IP = socket.gethostbyname(hostname)
+def main():
+    # Get the host machine's IP address
+    hostname = socket.gethostname()
+    UDP_IP = socket.gethostbyname(hostname)
 
-# Set up the server's port number
-UDP_PORT = 8080
+    # Set up the server's port number
+    UDP_PORT = 8080
 
-# Display the host machine's IP address
-print(f"Server IP address: {UDP_IP}")
+    # Display the host machine's IP address
+    print(f"Server IP address: {UDP_IP}")
 
-# Ask the user to confirm start the server
-server_start = input("Press 'y' to start the server: ")
-if server_start.lower() != 'y':
-    print("Server not started")
+    # Ask the user to confirm start the server
+    server_start = input("Press 'y' to start the server: ")
+    if server_start.lower() != 'y':
+        print("Server not started")
+        sys.exit()
+
+    # Create a socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the IP address and port number
+    sock.bind((UDP_IP, UDP_PORT))
+
+    # Receive the image file size from the client
+    data, addr = sock.recvfrom(1024)
+    file_size = int(data.decode())
+
+    # Receive the image file data from the client and write it to a file
+    received_data_size = 0
+    with open("test2.jpg", "wb") as f:
+        while received_data_size < file_size:
+            data, addr = sock.recvfrom(1028)
+            sequence_number = int(data[:4].decode())
+            data = data[4:]
+            f.write(data)
+            received_data_size += len(data)
+
+            # Send an acknowledgement back to the client
+            sock.sendto("ACK".encode(), addr)
+            print(f"Received packet {sequence_number}")
+
+    # Close the socket and shut down
+    sock.close()
+    print("Image received successfully")
+
+    # Exit on the console
+    input("Press enter to exit")
     sys.exit()
 
-# Create a socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Bind the socket to the IP address and port number
-sock.bind((UDP_IP, UDP_PORT))
-
-# Receive the image file size from the client
-data, addr = sock.recvfrom(1024)
-file_size = int(data.decode())
-
-# Receive the image file data from the client and write it to a file
-received_data_size = 0
-with open("test2.jpg", "wb") as f:
-    while received_data_size < file_size:
-        data, addr = sock.recvfrom(1024)
-        f.write(data)
-        received_data_size += len(data)
-
-# Close the socket and shut down
-sock.close()
-print("Image received successfully")
-
-# Exit on the console
-input("Press enter to exit")
-sys.exit()
+if __name__ == "__main__":
+    main()
